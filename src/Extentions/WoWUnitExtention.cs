b82a70a636537b53f84242951c250b60c6a2680d
Max Sychenko <max.itcn@gmail.com>
2017-05-04 15:13:32 +0300

    null validation IsCasting for unit

--- src/Extentions/WoWUnitExtention.cs
+++ src/Extentions/WoWUnitExtention.cs
@@ -6,7 +6,8 @@ namespace Zoth.Bot.CombatRoutine.Extentions
     {
         public static bool IsCasting(this WoWUnit unit)
         {
-             return unit.Casting > 0 || unit.Channeling > 0;
+            if (unit == null) return false;
+            return unit.Casting > 0 || unit.Channeling > 0;
         }
     }
 }
